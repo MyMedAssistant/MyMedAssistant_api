1bf4be4e0e9b8b9df795520bc551721c23058e01
Eugene Monnier <eugene.monnier@gmail.com>
2020-07-14 23:13:28 -0700

    created update_scheduler function

--- scheduler/apps.py
+++ scheduler/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
+import time
+import schedule
 
+from .update_schedule import update_time
 
 class SchedulerConfig(AppConfig):
-    name = 'scheduler'
+  name = 'scheduler'
+
+  def ready(self):
+    schedule.every().hour.at(':00').do(update_time)
+    schedule.every().hour.at(':15').do(update_time)
+    schedule.every().hour.at(':30').do(update_time)
+    schedule.every().hour.at(':45').do(update_time)
+    while True:
+      schedule.run_pending()
+      time.sleep(1)
